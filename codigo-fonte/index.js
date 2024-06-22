bfa066a70d9f1fc2a90da5c8c6afe83117923c35
NayanaLoivos <loivosdev@gmail.com>
2024-06-22 20:05:21 -0300

    now is working the entire integration

--- codigo-fonte/index.js
+++ codigo-fonte/index.js
@@ -1,24 +1,42 @@
-
 let articles = [
-    { id: 1, title: 'Chumbo (Pb)', description: 'O Perigo do Chumbo em Resíduos Eletrônicos', link: 'artigo1.html' },
-    { id: 2, title: 'Mercúrio (Hg)', description: 'Mercúrio em Dispositivos Eletrônicos: Impactos e Manejo.', link: 'artigo2.html' },
-    { id: 3, title: 'Cádmio (Cd)', description: 'Cádmio em Resíduos Eletrônicos: Um Metal Tóxico e Persistente.', link: 'artigo3.html' },
-    { id: 4, title: 'Bifenilos Policlorados (PCBs) em Equipamentos Eletrônicos Antigos', description: 'Bifenilos Policlorados (PCBs) em Equipamentos Eletrônicos Antigos', link: 'artigo4.html' },
-    { id: 5, title: 'Retardantes de Chama Bromados (BFRs)', description: 'Retardantes de Chama Bromados em Plásticos de Dispositivos Eletrônicos', link: 'artigo5.html' },
-    { id: 6, title: 'Ftalatos', description: 'Ftalatos em Cabos e Componentes Eletrônicos', link: 'artigo6.html' },
-    { id: 7, title: 'Arsênio (As)', description: 'Arsênio em Componentes Eletrônicos: Riscos e Reciclagem', link: 'artigo7.html' },
-    { id: 8, title: 'Níquel (Ni)', description: 'Content for Artigo 8', link: 'artigo8.html' }
+    {id: 1, title: 'Chumbo (Pb)', description: 'O Perigo do Chumbo em Resíduos Eletrônicos', link: 'artigo1.html'},
+    {
+        id: 2,
+        title: 'Mercúrio (Hg)',
+        description: 'Mercúrio em Dispositivos Eletrônicos: Impactos e Manejo.',
+        link: 'artigo2.html'
+    },
+    {
+        id: 3,
+        title: 'Cádmio (Cd)',
+        description: 'Cádmio em Resíduos Eletrônicos: Um Metal Tóxico e Persistente.',
+        link: 'artigo3.html'
+    },
+    {
+        id: 4,
+        title: 'Bifenilos Policlorados (PCBs) em Equipamentos Eletrônicos Antigos',
+        description: 'Bifenilos Policlorados (PCBs) em Equipamentos Eletrônicos Antigos',
+        link: 'artigo4.html'
+    },
+    {
+        id: 5,
+        title: 'Retardantes de Chama Bromados (BFRs)',
+        description: 'Retardantes de Chama Bromados em Plásticos de Dispositivos Eletrônicos',
+        link: 'artigo5.html'
+    },
+    {id: 6, title: 'Ftalatos', description: 'Ftalatos em Cabos e Componentes Eletrônicos', link: 'artigo6.html'},
+    {
+        id: 7,
+        title: 'Arsênio (As)',
+        description: 'Arsênio em Componentes Eletrônicos: Riscos e Reciclagem',
+        link: 'artigo7.html'
+    },
+    {id: 8, title: 'Níquel (Ni)', description: 'Content for Artigo 8', link: 'artigo8.html'}
 ];
 
-const pacienteLink = document.querySelector('#nav-bar-item-paciente');
-const minhaConta = document.getElementById('minha-conta');
+const pacienteLink = document.getElementById('informacao-pacientes');
 
-minhaConta.addEventListener('click', function (event){
-    event.preventDefault();
-    document.querySelector('.nav-bar').style.display = 'block';
-});
-
-pacienteLink.addEventListener('click', function(event) {
+pacienteLink.addEventListener('click', function (event) {
     event.preventDefault();
     window.location.href = "clients-inf.html";
 });
@@ -46,7 +64,7 @@ function addArticlesToGrid() {
 
 function addArticle(title, description, link) {
     const newId = articles.length + 1;
-    const newArticle = { id: newId, title, description, link };
+    const newArticle = {id: newId, title, description, link};
     articles.push(newArticle);
     addArticlesToGrid();
 }
